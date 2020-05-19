b3d48cb4e1b8fbad2b24e0d923d27162d813b6ae
jinjian5 <jinjian5@jd.com>
2020-05-19 18:12:40 +0800

    update

--- cloudbaseinit/metadata/services/localfileservice.py
+++ cloudbaseinit/metadata/services/localfileservice.py
@@ -37,6 +37,7 @@ class LocalFileService(baseopenstackservice.BaseOpenStackService):
         LOG.info('Init LocalFileService _get_data')
         # 读取文件数据
         norm_path = os.path.normpath(os.path.join(self._metadata_path, path))
+        LOG.info('_get_data %s', norm_path)
         try:
             with open(norm_path, 'rb') as stream:
                 return stream.read()
